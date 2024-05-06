9638e43a40b6af909d7180c8f970a0effffc6cc9
alexanderdombroski <dom21013@byui.edu>
2024-05-06 12:27:04 -0600

    Added calc circle function

--- shapes.py
+++ shapes.py
@@ -1,10 +1,16 @@
 # File to calculate shapes
 
+import math
+
+def calc_circle_area(radius):
+    return radius * radius * math.pi
+
 def square(side):
     return side * side
 
 def main() -> None:
-    print(square(5))
+    print(f"\n A square with a side length of 5 has an area of {square(5)}")
+    print(f"Circle with radius of 5 has area of {calc_circle_area(5):.2f}\n")
 
 if __name__ == "__main__":
     main()
