09d19bf5556c450f6df89889332b535e1ece5400
alexanderdombroski <dom21013@byui.edu>
2024-05-08 11:56:49 -0600

    added triange area function

--- shapes.py
+++ shapes.py
@@ -5,12 +5,17 @@ import math
 def calc_circle_area(radius):
     return radius * radius * math.pi
 
+def calc_triangle_area(base, height):
+    return 1/2 * base * height
+
 def square(side):
     return side * side
 
-def main() -> None:
+def main():
     print(f"\n A square with a side length of 5 has an area of {square(5)}")
-    print(f"Circle with radius of 5 has area of {calc_circle_area(5):.2f}\n")
+    print(f"Circle with radius of 5 has area of {calc_circle_area(5):.2f}")
+    print(f"A triangle with base of 5 has height of 5 has an area of {calc_triangle_area(5, 5):.2f}\n")
+
 
 if __name__ == "__main__":
     main()
